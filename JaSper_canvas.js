642e2757add47ac08a72d0cfc2b2e6f5bee27658
José M. Carnero <jm_carnero@sargazos.net>
2015-01-08 04:09:02 +0100

    canvas: escalado en animaciones y otras mejoras

--- JaSper_canvas.js
+++ JaSper_canvas.js
@@ -42,10 +42,6 @@ JaSper.funcs.extend(JaSper.prototype, {
 				if(this.JaSperItems == undefined)
 					this.JaSperItems = [];
 
-				if(props.id == undefined || props.id === false){
-					props.id =  this.JaSperItems.length;
-				}
-
 				//cada elemento de este array debe guardar todo lo necesario para volver a pintarlo si fuera necesario
 				this.JaSperItems[props.id] = props; //cada elemento tiene como nombre su id, y sus propiedades como un objeto
 
@@ -60,21 +56,24 @@ JaSper.funcs.extend(JaSper.prototype, {
 		};
 
 		//metodos disponibles
-		var validFuncs = {'circle': 'canvasCircle', 'image': 'canvasImage', 'path': 'canvasPath', 'polygon': 'canvasPolygon', 'text': 'canvasText'};
+		var validFuncs = {'background': 'canvasBackground', 'circle': 'canvasCircle', 'image': 'canvasImage', 'path': 'canvasPath', 'polygon': 'canvasPolygon', 'text': 'canvasText'};
 
 		for(var arg in arguments){
-			var func = false;
+			var id = false;
 			try{
-				func = Object.keys(arguments[arg])[0];
+				id = Object.keys(arguments[arg])[0];
 			}
 			catch(ex){}
 
-			if(func !== false && validFuncs[func] !== undefined){
-				arguments[arg][func].func = validFuncs[func]; //incluye el nombre de la funcion (que se guardara como parte de las propiedades del item) para posteriores usos
-				callFuncs(this, arguments[arg][func]);
+			if(id !== false && arguments[arg][id].func !== undefined){
+				if(validFuncs[arguments[arg][id].func] !== undefined){
+					arguments[arg][id].func = validFuncs[arguments[arg][id].func]; //se pone el nombre correcto de la funcion si se ha llamado a la abreviada
+				}
+				arguments[arg][id].id = id; //comodidad para siguientes usos
+				callFuncs(this, arguments[arg][id]);
 			}
 			else{
-				JaSper.funcs.log('-JaSper::canvas- submetodo desconocido: ' + arguments[arg], 2);
+				JaSper.funcs.log('-JaSper::canvas- id de objeto invalida', 2);
 				return false;
 			}
 		}
@@ -85,6 +84,7 @@ JaSper.funcs.extend(JaSper.prototype, {
 	/**
 	 * Animaciones
 	 *
+	 * @todo finalizar la animacion cuando no haya nada que animar (todos quietos o fuera y sin vuelta)
 	 * @since 2014-11-28
 	 * @param object props Propiedades del circulo {"id": "id_circulo", "centroX": nn, "centroY": nn, "radio": nn, "fondo": "color_fondo"}
 	 * @return boolean
@@ -134,7 +134,7 @@ JaSper.funcs.extend(JaSper.funcs, {
 		};
 
 		//metodos disponibles
-		var validFuncs = {'move': 'canvasMove'};
+		var validFuncs = {'move': 'canvasMove', 'scale': false};
 
 		for(var pr in props){
 			var func = false;
@@ -143,10 +143,40 @@ JaSper.funcs.extend(JaSper.funcs, {
 			}
 			catch(ex){}
 
+			//si hay limite de frames para este objeto y se ha alcanzado se salta
+			if(props[pr][func].frames !== undefined && props[pr][func].frames-- < 1)
+				continue;
+
 			var callProps = props[pr][func];
 			if(func !== false && validFuncs[func] !== undefined){
-				callProps.func = validFuncs[func];
-				callFuncs(canvas, callProps);
+				if(validFuncs[func] !== false){ //no todos los tipos de animacion son llamadas directas a metodos
+					callProps.func = validFuncs[func];
+					callFuncs(canvas, callProps);
+				}
+				else{
+					switch(func){
+						case 'scale': //TODO callbacks?
+							if(!callProps.id || !canvas.JaSperItems[callProps.id])
+								return false;
+							if(canvas.JaSperItems[callProps.id].scaleX === undefined) canvas.JaSperItems[callProps.id].scaleX = 0;
+							if(canvas.JaSperItems[callProps.id].scaleY === undefined) canvas.JaSperItems[callProps.id].scaleY = 0;
+
+							var scaleX = callProps.scaleX || 0;
+							var scaleY = callProps.scaleY || 0;
+
+							var speed = callProps.speed || 1; //velocidad, en pixels, del movimiento
+							if(typeof callProps.speed === 'function')
+								speed = callProps.speed.call(null); //callback
+
+							canvas.JaSperItems[callProps.id].scaleX += (scaleX * speed);
+							canvas.JaSperItems[callProps.id].scaleY += (scaleY * speed);
+
+							break;
+						default:
+							JaSper.funcs.log('-JaSper::canvasAnimate- submetodo desconocido: ' + func, 2);
+							return false;
+					}
+				}
 			}
 			else{
 				JaSper.funcs.log('-JaSper::canvasAnimate- submetodo desconocido: ' + props[pr], 2);
@@ -155,6 +185,29 @@ JaSper.funcs.extend(JaSper.funcs, {
 		}
 
 		JaSper.funcs.canvasRedraw(canvas);
+	},
+
+	/**
+	 * Fondo; color, degradado, imagen, ...
+	 * 
+	 * @since 2015-01-07
+	 * @param object canvas Objeto canvas a redibujar
+	 * @return boolean
+	 */
+	canvasBackground: function (context, props){
+		if(!context || !context.canvas) return false;
+
+		props.fillStyle = props.fillStyle || '#fff'; //color de fondo //TODO degradados, imagenes de fondo
+		props.width = props.width || context.canvas.style.width || false; //ancho del canvas
+		props.height = props.height || context.canvas.style.height || false; //alto del canvas
+
+		if(props.width) context.canvas.width = props.width;
+		if(props.height) context.canvas.height = props.height;
+
+		context.fillStyle = props.fillStyle; //limpia el canvas antes de redibujarlo; si no se limpia los objetos en movimiento dejan estela (no se borran las posiciones anteriores)
+		context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+
+		return true;
 	},
 
 	/**
@@ -177,10 +230,21 @@ JaSper.funcs.extend(JaSper.funcs, {
 		props.fill = props.fill || '#ccc';
 		props.borderWidth = props.borderWidth || 1;
 		props.border = props.border || '#3ab';
+		var x = props.x, y = props.y;
+
+		context.save();
+
+		//transforms
+		if(props.scaleX !== undefined || props.scaleY !== undefined){
+			context.translate(props.x, props.y);
+			x = props.r;
+			y = props.r;
+			JaSper.funcs.canvasScale(context, props);
+		}
 
 		context.beginPath();
 		//context.arc(x, y, r, (Math.PI/180) * grados_inicio_de_arco, (Math.PI/180) * grados_fin_de_arco, counterclockwise);
-		context.arc(props.x, props.y, props.r, (Math.PI/180) * props.angleStart, (Math.PI/180) * props.angleEnd, props.cclock);
+		context.arc(x, y, props.r, (Math.PI/180) * props.angleStart, (Math.PI/180) * props.angleEnd, props.cclock);
 		context.closePath();
 
 		context.fillStyle = props.fill; //relleno
@@ -190,6 +254,7 @@ JaSper.funcs.extend(JaSper.funcs, {
 		context.stroke();
 		context.fill();
 
+		context.restore();
 		return true;
 	},
 
@@ -233,12 +298,18 @@ JaSper.funcs.extend(JaSper.funcs, {
 		if(!props.id || !canvas.JaSperItems[props.id])
 			return false;
 
-		props.angle = props.angle || 0; //angulo del movimiento
-		var radianes = props.angle * Math.PI / 180;
-		props.speed = props.speed || 1; //velocidad, en pixels, del movimiento
+		var angle = props.angle || 0; //angulo del movimiento
+		if(typeof props.angle === 'function')
+			angle = props.angle.call(null); //callback
 
-		canvas.JaSperItems[props.id].x += (Math.cos(radianes) * props.speed);
-		canvas.JaSperItems[props.id].y += (Math.sin(radianes) * props.speed);
+		var radianes = angle * Math.PI / 180;
+
+		var speed = props.speed || 1; //velocidad, en pixels, del movimiento
+		if(typeof props.speed === 'function')
+			speed = props.speed.call(null); //callback
+
+		canvas.JaSperItems[props.id].x += (Math.cos(radianes) * speed);
+		canvas.JaSperItems[props.id].y += (Math.sin(radianes) * speed);
 
 		return true;
 	},
@@ -271,22 +342,33 @@ JaSper.funcs.extend(JaSper.funcs, {
 
 		props.sides = props.sides || 3; //numero de lados //TODO 0 -> circulo
 		props.r = props.r || 50; //radio de la circunferencia para los vertices del poligono //TODO por defecto debe ser la media del alto y ancho del canvas o similar
-		props.x = props.x || 100; //centro, coordenada x
-		props.y = props.y || 100; //centro, coordenada y
+		props.x = props.x || 0; //centro, coordenada x
+		props.y = props.y || 0; //centro, coordenada y
 		props.angle = props.angle || 0; //
 		props.cclock = props.cclock || false; //se dibuja en el sentido de las agujas del reloj o al contrario
 		props.fill = props.fill || '#ccc';
 		props.borderWidth = props.borderWidth || 1;
 		props.border = props.border || '#3ab';
+		var x = props.x, y = props.y;
+
+		context.save();
+
+		//transforms
+		if(props.scaleX !== undefined || props.scaleY !== undefined){
+			context.translate(props.x, props.y);
+			x = props.r;
+			y = props.r;
+			JaSper.funcs.canvasScale(context, props);
+		}
 
 		context.beginPath(); //inicia el poligono
 
 		//calcula las posiciones de los vertices y comienza el path
-		context.moveTo(props.x + props.r * Math.sin(props.angle), props.y - props.r * Math.cos(props.angle));
+		context.moveTo(x + props.r * Math.sin(props.angle), y - props.r * Math.cos(props.angle));
 		var delta = 2 * Math.PI / props.sides; //angulo entre vertices
 		for(var i = 1;i < props.sides;i++){ //resto de vertices
 			props.angle += props.cclock ? -delta : delta; //angulo de este vertice
-			context.lineTo(props.x + props.r * Math.sin(props.angle), props.y - props.r * Math.cos(props.angle)); //calcula la posicion del vertice y dibuja una linea hasta el
+			context.lineTo(x + props.r * Math.sin(props.angle), y - props.r * Math.cos(props.angle)); //calcula la posicion del vertice y dibuja una linea hasta el
 		}
 		context.closePath(); //cierra el poligono
 
@@ -301,7 +383,8 @@ JaSper.funcs.extend(JaSper.funcs, {
 
 		context.fill(); //pinta el relleno
 		context.stroke(); //pinta el borde
-	
+
+		context.restore();
 		return true;
 	},
 
@@ -319,8 +402,6 @@ JaSper.funcs.extend(JaSper.funcs, {
 		}
 
 		var context = canvas.getContext('2d');
-		context.fillStyle = '#fff'; //TODO limpia el canvas antes de redibujarlo, parametrizar este color de fondo; si no se limpia los objetos en movimiento dejan estela (no se borran las posiciones anteriores); tal vez el fondo deberia ser un objeto mas
-		context.fillRect(0, 0, canvas.width, canvas.height);
 
 		var items = canvas.JaSperItems;
 		for(var item in items){
@@ -328,6 +409,31 @@ JaSper.funcs.extend(JaSper.funcs, {
 			if(typeof JaSperFunc === 'function')
 				JaSperFunc.call(null, context, items[item]); //TODO decidir que hacer con el retorno de la funcion
 		}
+
+		return true;
+	},
+
+	/**
+	 * Escala (cambia tamano) objetos
+	 * 
+	 * @since 2015-01-07
+	 * @param object canvas Canvas object
+	 * @param object props Propiedades
+	 * @return boolean
+	 */
+	canvasScale: function (context, props){
+		if(!context || !context.canvas) return false;
+
+		if(!props)
+			var props = {};
+
+		/*if(!props.id || !context.canvas.JaSperItems[props.id])
+			return false;*/
+		var scaleX = props.scaleX || 1; //TODO callbacks?
+		var scaleY = props.scaleY || 1;
+
+		//context.setTransform(1,0,0,1,0,0); //identity matrix
+		context.scale(scaleX, scaleY); //x scale, y scale
 
 		return true;
 	},
@@ -354,11 +460,23 @@ JaSper.funcs.extend(JaSper.funcs, {
 			props.font = props.fontItalic + ' ' + props.fontWeight + ' ' + props.fontSize + ' ' + props.fontName;
 		}
 		props.fillText = props.fillText || 'Hello world!';
+		var x = props.x, y = props.y;
+
+		context.save();
+
+		//transforms
+		if(props.scaleX !== undefined || props.scaleY !== undefined){
+			context.translate(props.x, props.y);
+			x = props.r;
+			y = props.r;
+			JaSper.funcs.canvasScale(context, props);
+		}
 
 		context.fillStyle = props.fillStyle;
 		context.font = props.font;
-		context.fillText(props.fillText, props.x, props.y);
+		context.fillText(props.fillText, x, y);
 
+		context.restore();
 		return true;
 	}
 });
